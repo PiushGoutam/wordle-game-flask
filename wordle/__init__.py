4a2b79a0cb76cf01b97ae62bb8eae10f01a0d7a3
unknown <piushgoutam@gmail.com>
2026-01-30 01:04:30 +0530

    some more changes before I call it a night

--- wordle/__init__.py
+++ wordle/__init__.py
@@ -5,9 +5,9 @@ def create_app():
 
     @app.route('/')
     def index():        
-        keylist = {'first_row':["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-                   'second_row':["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-                   'third_row':["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DEL"]
-                   }        
+        keylist = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+                   ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+                   ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DEL"]
+        ]    
         return render_template('game-page.html', keylist=keylist)
     return app
